30e4df18fc2cf2f4bdc57b20fae707d325a1ceef
iamjahnavi2010 <78913699+iamjahnavi2010@users.noreply.github.com>
2022-01-19 16:49:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function preload()
 {
@@ -24,6 +26,19 @@ function modelLoaded() {
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+ if(scoreLeftWrist > 0.2)
+ {
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY=Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume=remove_decimals/500;
+    document.getElementById("volume").innerHTML = "Volume = " + volume;
+    song.setVolume(volume);
+ }
+    
 }
 
 function play()
@@ -38,11 +53,14 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+            
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
 
-        console.log(results);
+        
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
